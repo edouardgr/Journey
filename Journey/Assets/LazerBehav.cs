a6f6e3197c182696f4f1d438ed831f89020dbabe
edouardgr <edouard.gruyters@gmail.com>
2020-01-31 13:28:55 -0500

    Laser Update

--- Journey/Assets/LazerBehav.cs
+++ Journey/Assets/LazerBehav.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class LazerBehav : MonoBehaviour
 {
     public float spin_rate = 2f;
-    LineRenderer lr_up, lr_down;
+    LineRenderer lr;
 
     // Start is called before the first frame update
     void Start()
     {
-        lr_up = transform.GetChild(0).GetComponent<LineRenderer>();
-        lr_up.positionCount = 2;
-        lr_down = transform.GetChild(1).GetComponent<LineRenderer>();
-        lr_down.positionCount = 2;
+        lr = GetComponent<LineRenderer>();
+        lr.positionCount = 2;
     }
 
     private void FixedUpdate()
@@ -26,21 +24,11 @@ public class LazerBehav : MonoBehaviour
     {
         RaycastHit2D hit_up = Physics2D.Raycast(transform.position, transform.up);
         if(hit_up) {
-            lr_up.SetPosition(0, transform.position);
-            lr_up.SetPosition(1, hit_up.point);
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, hit_up.point);
 
             if(hit_up.collider.tag == "Player") {
                 hit_up.collider.GetComponent<MoveMonster>().Respawn();
-            }
-        }
-
-        RaycastHit2D hit_down = Physics2D.Raycast(transform.position, -transform.up);
-        if (hit_down) {
-            lr_down.SetPosition(0, transform.position);
-            lr_down.SetPosition(1, hit_down.point);
-
-            if (hit_down.collider.tag == "Player") {
-                hit_down.collider.GetComponent<MoveMonster>().Respawn();
             }
         }
     }
