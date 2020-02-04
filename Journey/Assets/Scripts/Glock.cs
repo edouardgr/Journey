127fd36986c6ae36672078be979f60fdf245a473
edouardgr <edouard.gruyters@gmail.com>
2020-02-03 21:37:16 -0500

    up

---
+++ Journey/Assets/Scripts/Glock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glock : MonoBehaviour
+{
+    //public Variables
+    public float speed = 5.0f;//USed for how fast the bullet will be
+
+    //Private Variables 
+    private float _canfire = -1f;//used as comparasion for when the bullet can fire 
+    private float _firerate = 0.05f;//used for delay until the player can fire again
+    public GameObject _bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //0 for left click, 1 for right click, 2 for middle button
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed primary button.");//Debug purpose so that it works
+            //Time.time used for how long the game has been running
+            _canfire = Time.time + _firerate; //Cool down system
+            //Debug.Log("Space Key Pressed"); //Debug log to verify 
+            Instantiate(_bulletPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity); //Clones the original and returns the clone 
+
+        }
+
+    }
+}
