f4f953792a65d1c275ba2fc6c26adbc6de689384
edouardgr <edouard.gruyters@gmail.com>
2020-02-07 14:42:50 -0500

    Added playerController

--- Journey/Assets/Scripts/CameraController.cs
+++ Journey/Assets/Scripts/CameraController.cs
@@ -7,7 +7,6 @@ public class CameraController : MonoBehaviour
      public float HorizontalSpeed = 4.0f;
      public float VerticalSpeed = 4.0f;
 
-
      private float yaw = 0.0f;
      private float pitch = 0.0f;
     // Update is called once per frame
