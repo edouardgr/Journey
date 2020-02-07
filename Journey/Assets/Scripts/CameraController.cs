4f39422c9cf974dfe4ff55c98389f29a962b7eb0
Brandon <bdupree45@gmail.com>
2020-02-07 14:54:04 -0500

    Raycast

---
+++ Journey/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+     public float HorizontalSpeed = 4.0f;
+     public float VerticalSpeed = 4.0f;
+
+     private float yaw = 0.0f;
+     private float pitch = 0.0f;
+    // Update is called once per frame
+    void Update()
+    {
+          yaw += HorizontalSpeed * Input.GetAxis("Mouse X");
+          pitch -= VerticalSpeed * Input.GetAxis("Mouse Y");
+
+          transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
--- Journey/Assets/Scripts/Glock.cs
+++ Journey/Assets/Scripts/Glock.cs
@@ -10,12 +10,6 @@ public class Glock : MonoBehaviour
     private float _firerate = 0.05f;//used for delay until the player can fire again
     public GameObject _bulletPrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -27,6 +21,28 @@ public class Glock : MonoBehaviour
             _canfire = Time.time + _firerate; //Cool down system
             //Debug.Log("Space Key Pressed"); //Debug log to verify 
             Instantiate(_bulletPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity); //Clones the original and returns the clone 
+
+            //Raycasting attempt
+
+            //Bit shift the index of the layer (8) to get a bit mask
+            int layerMask = 1 << 8;
+
+            //This would cast rays only against colliders in layer 8.
+            //But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+            layerMask = ~layerMask;
+
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit");
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+                Debug.Log("Did not Hit");
+            }
 
         }
 
