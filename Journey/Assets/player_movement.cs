1f53ea5478a5c0ed4cbeb61c0fe0eb7ffea72357
edouardgr <edouard.gruyters@gmail.com>
2020-02-07 23:35:06 -0500

    Made the best movement ever

--- Journey/Assets/player_movement.cs
+++ Journey/Assets/player_movement.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class player_movement : MonoBehaviour
 {
     CharacterController cc;
-    public float speed = 5f;
+    public float max_speed = 5f;
+    public float acc_input_x = 0.1f, acc_input_z = 0.1f;
+    public float curr_input_x = 0f, curr_input_z = 0f;
 
     // Start is called before the first frame update
     void Awake()
@@ -20,19 +22,40 @@ public class player_movement : MonoBehaviour
         // Get Horizontal and Vertical Input
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
+        
+        //Forward movement
+        if (verticalInput != 0f) {
+            curr_input_z += acc_input_z * verticalInput;
+        } else {
+            if (Mathf.Abs(curr_input_z) > acc_input_z) {
+                curr_input_z -= acc_input_z * (curr_input_z > 0f ? 1f : -1f);
+            } else {
+                curr_input_z = 0f;
+            }
+        }
+        curr_input_z = Mathf.Clamp(curr_input_z, -1, 1);
+
+        //Sideways movement
+        if (horizontalInput != 0f) {
+            curr_input_x += acc_input_x * horizontalInput;
+        } else {
+            if (Mathf.Abs(curr_input_x) > acc_input_x) {
+                curr_input_x -= acc_input_x * (curr_input_x > 0f ? 1f : -1f);
+            } else {
+                curr_input_x = 0f;
+            }
+        }
+        curr_input_x = Mathf.Clamp(curr_input_x, -1, 1);
 
         // Calculate the Direction to Move based on the tranform of the Player
-        Vector3 moveDirectionForward = transform.forward * verticalInput;
-        Vector3 moveDirectionSide = transform.right * horizontalInput;
+        Vector3 moveDirectionForward = transform.forward * curr_input_z * max_speed;
+        Vector3 moveDirectionSide = transform.right * curr_input_x * max_speed;
 
         //find the direction
-        Vector3 direction = (moveDirectionForward + moveDirectionSide).normalized;
+        Vector3 direction = (moveDirectionForward + moveDirectionSide);
 
         //find the distance
-        Vector3 distance = direction * speed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.LeftShift)) {
-            distance *= speed;
-        }
+        Vector3 distance = direction * Time.deltaTime;
 
         //Gravity
         distance.y += Physics.gravity.y * Time.deltaTime;
