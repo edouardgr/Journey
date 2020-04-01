f3bba0e392f727745f4f304c1bf98ccd71e0cbaa
Nazey <bmassino120@gmail.com>
2020-04-01 00:34:07 -0400

    Added bobbing water mechanic, made movement script smoother

--- Journey/Assets/Scripts/Player/Player_Movement1.cs
+++ Journey/Assets/Scripts/Player/Player_Movement1.cs
@@ -7,12 +7,17 @@ public class Player_Movement1 : MonoBehaviour
 
      float speed = 12.0f;
      float jump_speed = 9.0f;
-     float gravity = 20.0f;
+     float gravity = 25.0f;
      public bool launch = false;
 
      float swim_speed = 12f;
      float swim_gravity = 3f;
      bool is_in_water = false;
+
+     //Vertical and Horizontal movement
+     public float acc_input_x = 0.2f, acc_input_z = 0.2f;
+     public float curr_input_x = 0f, curr_input_z = 0f;
+
 
      public Vector3 move_direction = Vector3.zero;
      Vector3 move_direction_water = Vector3.zero;
@@ -25,16 +30,64 @@ public class Player_Movement1 : MonoBehaviour
           Cursor.lockState = CursorLockMode.Locked;
      }
 
+     void FixedUpdate()
+     {
+          // Get Horizontal and Vertical Input
+          float horizontalInput = (Input.GetAxisRaw("Horizontal"));
+          float verticalInput = (Input.GetAxisRaw("Vertical"));
 
+          //Forward movement
+          if (verticalInput != 0f)
+          {
+               curr_input_z += acc_input_z * verticalInput;
+          }
+          else
+          {
+               if (Mathf.Abs(curr_input_z) > acc_input_z)
+               {
+                    curr_input_z -= acc_input_z * (curr_input_z > 0f ? 1f : -1f);
+               }
+               else
+               {
+                    curr_input_z = 0f;
+               }
+          }
+          curr_input_z = Mathf.Clamp(curr_input_z, -1, 1);
+
+          //Sideways movement
+          if (horizontalInput != 0f)
+          {
+               curr_input_x += acc_input_x * horizontalInput;
+          }
+          else
+          {
+               if (Mathf.Abs(curr_input_x) > acc_input_x)
+               {
+                    curr_input_x -= acc_input_x * (curr_input_x > 0f ? 1f : -1f);
+               }
+               else
+               {
+                    curr_input_x = 0f;
+               }
+          }
+          curr_input_x = Mathf.Clamp(curr_input_x, -1, 1);
+     }
      void Update()
      {
+     
           if (is_in_water)
           {
                move_direction_water = Vector3.zero;
-               move_direction_water += Input.GetAxis("Vertical") * transform.GetChild(0).forward * swim_speed;
-               move_direction_water += Input.GetAxis("Horizontal") * transform.GetChild(0).right * swim_speed;
+               move_direction_water +=  curr_input_z * transform.GetChild(0).forward * swim_speed;
+               move_direction_water += curr_input_x * transform.GetChild(0).right * swim_speed;
+               
+               //if space is held, let player swim to surface vertically
+               if (Input.GetKey("space"))
+               {
+                    move_direction_water += transform.GetChild(0).up *(swim_speed/2);
 
-               move_direction_water.y -= swim_gravity;
+               } else move_direction_water.y -= swim_gravity;
+
 
                CC.Move(move_direction_water * Time.deltaTime);
 
@@ -45,7 +98,7 @@ public class Player_Movement1 : MonoBehaviour
                if (CC.isGrounded && !launch)
                {
 
-                    move_direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                    move_direction = new Vector3(curr_input_x, 0, curr_input_z);
                     move_direction = transform.TransformDirection(move_direction);
                     move_direction *= speed;
 
@@ -58,8 +111,9 @@ public class Player_Movement1 : MonoBehaviour
                }
                else
                {
+                    //player is in the air, so change launched to false, as they cant be launched mid-air
                     launch = false;
-                    move_direction = new Vector3(Input.GetAxis("Horizontal"), move_direction.y, Input.GetAxis("Vertical"));
+                    move_direction = new Vector3(curr_input_x, move_direction.y, curr_input_z);
                     move_direction = transform.TransformDirection(move_direction);
                     move_direction.x *= speed;
                     move_direction.z *= speed;
@@ -81,9 +135,17 @@ public class Player_Movement1 : MonoBehaviour
      //When user jumps out of some water
      private void OnTriggerExit(Collider other)
      {
-          if (other.tag == "Water")
+          if (other.tag == "Water") {
                is_in_water = false;
 
+               //Makes jumping out of the water much smoother, less jagged
+               if (Input.GetKey("space"))
+               {
+                    move_direction.y = jump_speed/2;
+
+               }
+          }
+        
      }
 }
 
