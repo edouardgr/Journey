3d723da703bbf648515d29e036f4cec974707296
garrfield9240 <austingaudio@gmail.com>
2020-04-13 10:53:20 -0400

    stuff

--- Journey/Assets/Scripts/Interactive/LaunchPad.cs
+++ Journey/Assets/Scripts/Interactive/LaunchPad.cs
@@ -6,17 +6,30 @@ public class LaunchPad : MonoBehaviour
 {
       public float Object_Launch_Force = 75f;
       public float Player_Launch_Force = 25f;
-    
+	  public AudioClip triggerSound;
+	  private bool entered = true;
+	  AudioSource audioSource;
+	 void Start() {
+		 audioSource = GetComponent<AudioSource>();
+	 }
+	 void Update() {
+		 if (triggerSound != null) {
+			audioSource.PlayOneShot(triggerSound, 0.7F);
+		 } 
+	 }
      private void OnTriggerEnter(Collider other)
      {
-          
+		  if (triggerSound != null && entered == true) {
+				audioSource.PlayOneShot(triggerSound, 0.7F);
+		  }
           if (other.tag == "Player") {
 
                other.GetComponent<Player_Movement1>().move_direction.y = Player_Launch_Force;
                other.GetComponent<Player_Movement1>().launch = true;
-          }
-     }
 
+          }
+		  
+     }
      private void OnTriggerStay(Collider other)
      {
           if (other.tag == "Movable")
