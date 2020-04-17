f66d53491fe86f97b4200a7b347bdd6ee73146cf
edouardgr <edouard.gruyters@gmail.com>
2020-04-18 01:57:14 +0200

    Checkpoints created

---
+++ Journey/Assets/Editor/CheckpointEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Checkpoint_master))]
+public class CheckpointEditor : Editor
+{
+    GameObject prefab;
+    public override void OnInspectorGUI()
+    {
+        prefab = Resources.Load<GameObject>("Checkpoint");
+        Checkpoint_master obj = (Checkpoint_master)target;
+
+        if (GUILayout.Button("Add Checkpoint"))
+        {
+            GameObject gameObj = Instantiate(prefab, obj.transform.position, obj.transform.rotation, obj.transform);
+            gameObj.name = "Checkpoint " + obj.transform.childCount;
+        }
+
+        serializedObject.Update();
+    }
+}
