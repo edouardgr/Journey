687e4dad275810aadc37dfe3c31d23ce877d2a64
Nazey <bmassino120@gmail.com>
2020-04-17 17:22:58 -0400

    3x3 Puzzle Update
    Finished 3x3 puzzle, just be sure to put the player in "The Player" variable in the teleport script (it is marked). Also added Bailey's pause menu stuff.

---
+++ Journey/Assets/Scripts/Puzzle/3x3 Puzzle/Enable_Teleport_Zone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enable_Teleport_Zone : MonoBehaviour
+{
+     public BoxCollider Turn_Me_On;
+
+     private void OnTriggerEnter(Collider other)
+     {
+          Turn_Me_On.enabled = true;
+          GetComponent<BoxCollider>().enabled = false;
+     }
+}
