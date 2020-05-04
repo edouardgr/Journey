beaa64c952eb8389e308d9534245070bdbb2b476
edouardgr <edouard.gruyters@gmail.com>
2020-05-04 20:16:54 +0200

    Revert "Merge branch 'master' into Tony"
    This reverts commit 7a91e0468df048239bfcc814ee54eac5a3a75c2f, reversing
    changes made to ef0923157c224bf7fbc48e348c3dfa4742fc9f39.

---
+++ Journey/Assets/AI Graph/DataTypes/T_Type.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public abstract class T_Type
+{
+    public abstract void Draw();
+}
---
+++ Journey/Assets/AI Graph/Node/Init.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : Node
+{
+    public Init()
+    {
+        is_protected = true;
+        has_input = null;
+        color = Color.red;
+    }
+
+    public override void Start()
+    {
+
+    }
+
+    public override bool Update()
+    {
+        return true;
+    }
+}
